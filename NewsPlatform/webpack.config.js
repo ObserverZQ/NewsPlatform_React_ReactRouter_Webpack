f83ecdf1942f62830a3aa59ea8938242960fa27b
ObserverZQ <657282348@qq.com>
2018-04-08 17:35:14 +0800

    webpack compressed & packed(with sourcemap) successfully!!!yay!!

--- NewsPlatform/webpack.config.js
+++ NewsPlatform/webpack.config.js
@@ -1,14 +1,23 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
-
+const BabiliPlugin = require("babili-webpack-plugin");
 module.exports = {
     devServer: {
-      historyApiFallback: true
+      historyApiFallback: true //report specific errors in console
     },
+    // performance:{
+    //     hints: 'warning', // the level of file, throw a warning if suprass limit
+    //     maxEntrypointSize: 100000, //bytes, the maximum size of bundle.js
+    //     maxAssetSize: 450000  //size of images, css...
+    // },
     context: path.join(__dirname),
-    devtool: debug ? "inline-sourcemap" : null,
-    entry: "./src/js/root.js",
+    // devtool: debug ? "inline-sourcemap" : null,
+    devtool: "source-map",
+    entry: {
+        app: "./src/js/root.js",
+        vendor: ['react']
+    },
     module: {
         loaders: [
             {
@@ -33,11 +42,24 @@ module.exports = {
     },
     output: {
         path: __dirname,
-        filename: "./src/bundle.js"
+        filename: "[name].js"
     },
-    plugins: debug ? [] : [
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-    ],
+    // plugins: debug ? [] : [
+    //     new webpack.optimize.DedupePlugin(),
+    //     new webpack.optimize.OccurenceOrderPlugin(),
+    //     new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    //     new BabiliPlugin(),
+    //     new webpack.optimize.CommonsChunkPlugin({
+    //         name: 'vendor'
+    //     })
+    // ]
+    plugins: [
+        // new webpack.optimize.DedupePlugin(),
+        // new webpack.optimize.OccurenceOrderPlugin(),
+        // new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+        new BabiliPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        })
+    ]
 };
